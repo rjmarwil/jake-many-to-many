3813ff48d60ee53c7205fe7a145dc6ac9af6ba31
Jake Marwil <rjm02006@gmail.com>
2015-02-25 14:17:45 -0700

    add create method and define strong params

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -8,10 +8,19 @@ class ProductsController < ApplicationController
     @product = Product.new
   end
 
-
-    def create
-
-      ### create a product here.
+  def create
+    if current_user
+      @product = Product.new(product_params)
+      @product.save
+      redirect_to products_path, notice: 'Product was successfully created.'
+    else
+      redirect_to login_path, notice: "Must be logged in to create a product"
     end
+  end
+
+  private
+  def product_params
+    params.require(:product).permit(:name, :price, :company_id)
+  end
 
 end
