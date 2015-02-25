e53515406b24498b120159a0b5ce45e00505cf37
Jake Marwil <rjm02006@gmail.com>
2015-02-25 14:41:45 -0700

    add create method and define strong params

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -8,4 +8,20 @@ class CompaniesController < ApplicationController
     @company = Company.new
   end
 
+  def create
+    if current_user
+      @company = Company.new(company_params)
+      @company.save
+      redirect_to companies_path, notice: 'Company was successfully created.'
+    else
+      redirect_to login_path, notice: "Must be logged in to create a company"
+    end
+  end
+
+  private
+
+  def company_params
+    params.require(:company).permit(:name, :catch_phrase, :suffix)
+  end
+
 end
